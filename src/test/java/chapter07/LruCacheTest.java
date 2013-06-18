a84124ae1bad0cffcd51dcafcb4d36cd722f9934
m_yoshida <m_yoshida@e-mind.co.jp>
2013-06-18 18:22:16 +0900

    first commit

---
+++ src/test/java/chapter07/LruCacheTest.java
@@ -0,0 +1,14 @@
+package chapter07;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LruCacheTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
